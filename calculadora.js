b90772b60e67bcd876ee9539477ff069e6d1aa8c
dhiulya-pilonetto <dhiulyapilonetto@gmail.com>
2023-10-25 16:48:52 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,31 @@
+// variáveis
+let resultado = saldoDeVitorias (100, 17)
+let nivel = " "
+
+
+// função
+function saldoDeVitorias (numA, numB) {
+    let vitorias = numA - numB
+    return vitorias
+    classificacao()
+}
+
+// estrutura de decisão
+    if (resultado <= 10) {
+        nivel = "Ferro";
+    } else if (resultado >= 11 && resultado <= 20){
+        nivel = "Bronze";
+    } else if (resultado >= 21 && resultado <= 50) {
+        nivel = "Prata";
+    } else if (resultado >= 51 && resultado <= 80) { 
+        nivel = "Ouro";
+    } else if (resultado >= 81 && resultado <= 90) {
+        nivel = "Diamante";
+    } else if (resultado >= 91 && resultado <= 100) {
+        nivel = "Lendário";
+    } else if (resultado >= 100){
+        nivel = "Imortal";
+    }
+
+// saída
+console.log("O herói tem um saldo de vitórias de " + resultado + " e está no nível " + nivel)
